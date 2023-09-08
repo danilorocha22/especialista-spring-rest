3f738e0e92e981a7229b4fc192ff6fbdb60663fb
Danilo Rocha <dan@pop-os.localdomain>
2023-09-07 23:14:51 -0300

    subindo proejto

---
+++ src/main/java/com/dan/esr/EspecialistaSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.dan.esr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspecialistaSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspecialistaSpringRestApplication.class, args);
+	}
+
+}
