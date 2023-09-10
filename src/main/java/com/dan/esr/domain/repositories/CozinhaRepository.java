1beeb8b6af9f767770c9e7907ca21568b14ced2b
dan <danilo.rochaa@gmail.com>
2023-09-10 12:49:19 -0300

    Criação do repositório de Cozinha.

---
+++ src/main/java/com/dan/esr/domain/repositories/CozinhaRepository.java
@@ -0,0 +1,9 @@
+package com.dan.esr.domain.repositories;
+
+import com.dan.esr.domain.entities.Cozinha;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+}
