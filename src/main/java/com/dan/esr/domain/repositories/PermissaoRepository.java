cec904815de97e6ee09166bbe973ec89bf392b53
dan <danilo.rochaa@gmail.com>
2023-09-10 12:54:33 -0300

    Criação do repositório de Permissão.

---
+++ src/main/java/com/dan/esr/domain/repositories/PermissaoRepository.java
@@ -0,0 +1,9 @@
+package com.dan.esr.domain.repositories;
+
+import com.dan.esr.domain.entities.Permissao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
+}
