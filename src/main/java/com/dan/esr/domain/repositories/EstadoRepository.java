08d76fec2f991ff8de993a20b4c0587f72be98f5
dan <danilo.rochaa@gmail.com>
2023-09-16 21:43:46 -0300

    Criação do repositório de Estado.

---
+++ src/main/java/com/dan/esr/domain/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.dan.esr.domain.repositories;
+
+import com.dan.esr.domain.entities.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+}
