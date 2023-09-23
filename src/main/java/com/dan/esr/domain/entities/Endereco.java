1391fbdc01468b148d0f8facaf01b1c5d9d3a143
dan <danilo.rochaa@gmail.com>
2023-09-23 18:32:46 -0300

    Ajustando a classe Endereço

--- src/main/java/com/dan/esr/domain/entities/Endereco.java
+++ src/main/java/com/dan/esr/domain/entities/Endereco.java
@@ -1,18 +1,38 @@
 package com.dan.esr.domain.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
 @Embeddable
-public class Endereco implements Serializable {
+public class Endereco {
 
+    @Column(name = "endereco_logradouro")
     private String logradouro;
+
+    @Column(name = "endereco_numero")
     private String numero;
+
+    @Column(name = "endereco_complemento")
     private String complemento;
+
+    @Column(name = "endereco_bairro")
     private String bairro;
+
+    @Column(name = "endereco_cep")
     private String cep;
 
+    @ManyToOne
+    @JoinColumn(name = "endereco_cidade_id")
+    private Cidade cidade;
 
+    @ManyToOne
+    @JoinColumn(name = "endereco_estado_id")
+    private Estado uf;
 
 }
