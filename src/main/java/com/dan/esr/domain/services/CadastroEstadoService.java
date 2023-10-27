0e5535f287c754b6659aa6468c5568180ac2bac5
dan <danilo.rochaa@gmail.com>
2023-10-27 11:16:47 -0300

    Criação do serviço para cadastro de estado

---
+++ src/main/java/com/dan/esr/domain/services/CadastroEstadoService.java
@@ -0,0 +1,49 @@
+package com.dan.esr.domain.services;
+
+import com.dan.esr.domain.entities.Estado;
+import com.dan.esr.domain.exceptions.EntidadeEmUsoException;
+import com.dan.esr.domain.exceptions.EntidadeNaoEncontradaException;
+import com.dan.esr.domain.repositories.EstadoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import static com.dan.esr.domain.util.ValidarCamposObrigatoriosUtil.validarCampoObrigatorio;
+
+@AllArgsConstructor
+@Service
+public class CadastroEstadoService {
+
+    private static final String MSG_ESTADO_EM_USO = "Estado com ID %s, está em uso com " +
+            "e não pode ser excluído";
+    public static final String MSG_ESTADO_NAO_ENCONTRADO_COM_ID = "Estado não encontrada com ID %s";
+
+    private EstadoRepository estadoRepo;
+
+    public Estado buscarEstadoPorId(Long id) {
+        validarCampoObrigatorio(id, "ID");
+        return this.estadoRepo.findById(id).orElseThrow(()-> new EntidadeNaoEncontradaException(
+                String.format(MSG_ESTADO_NAO_ENCONTRADO_COM_ID, id)
+        ));
+    }
+
+    public Estado salvar(Estado estado) {
+        validarCampoObrigatorio(estado, "Estado");
+        return this.estadoRepo.save(estado);
+    }
+
+    public void remover(Long id) {
+        validarCampoObrigatorio(id, "ID");
+        Estado estadoRegistro = this.buscarEstadoPorId(id);
+
+        try {
+            this.estadoRepo.delete(estadoRegistro);
+        }catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(String.format(MSG_ESTADO_EM_USO, id));
+        }
+    }
+
+
+
+
+}
