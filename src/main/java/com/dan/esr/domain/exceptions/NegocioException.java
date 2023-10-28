189bcfb8d854714441d40c0af08a5a1c82e606d5
dan <danilo.rochaa@gmail.com>
2023-10-28 19:17:25 -0300

    Criação da classe NegocioException para tratar erros que retornam status http 400

---
+++ src/main/java/com/dan/esr/domain/exceptions/NegocioException.java
@@ -0,0 +1,16 @@
+package com.dan.esr.domain.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class NegocioException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public NegocioException(String message) {
+        super(message);
+    }
+}
