400e81a8260a8aa6eb10514ef486a7c0b9e0d571
dan <danilo.rochaa@gmail.com>
2023-10-31 10:51:51 -0300

    Refatoração

--- src/main/java/com/dan/esr/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/dan/esr/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,40 +3,49 @@ package com.dan.esr.api.exceptionhandler;
 import com.dan.esr.domain.exceptions.EntidadeEmUsoException;
 import com.dan.esr.domain.exceptions.EntidadeNaoEncontradaException;
 import com.dan.esr.domain.exceptions.NegocioException;
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Objects;
 
 import static com.dan.esr.api.exceptionhandler.Problema.novoProblema;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(novoProblema(e));
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(novoProblema(e));
-    }
-
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                .body(novoProblema("O tipo de mídia não é aceito"));
+    public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(novoProblema(e));
+    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(@NonNull Exception ex, Object body, @NonNull HttpHeaders headers,
+                                                             @NonNull HttpStatusCode status, @NonNull WebRequest request) {
+        if(body == null) {
+            body = novoProblema(ex.getMessage());
+        } else if (body instanceof String) {
+            body = novoProblema((String) body);
+        } else {
+            body = novoProblema(ex.getMessage());
+        }
 
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
 }
