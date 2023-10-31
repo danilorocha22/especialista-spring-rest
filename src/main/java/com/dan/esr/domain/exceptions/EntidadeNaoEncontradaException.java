98bd3478e50baa44628ec61e2ad6b058a8d92a36
dan <danilo.rochaa@gmail.com>
2023-10-31 09:51:29 -0300

    Refatoração

--- src/main/java/com/dan/esr/domain/exceptions/EntidadeNaoEncontradaException.java
+++ src/main/java/com/dan/esr/domain/exceptions/EntidadeNaoEncontradaException.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.Serial;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
-public class EntidadeNaoEncontradaException extends RuntimeException {
+//@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
+public class EntidadeNaoEncontradaException extends NegocioException {
     @Serial
     private static final long serialVersionUID = 1L;
 
