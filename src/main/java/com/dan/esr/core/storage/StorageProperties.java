9118f988fbf25385330c705bac69d12ccbb6fff2
Danilo Rocha <danilo.rochaa@gmail.com>
2024-06-13 22:04:01 -0300

    Criando bean de propriedades de configuração dos serviços de storage

---
+++ src/main/java/com/dan/esr/core/storage/StorageProperties.java
@@ -0,0 +1,39 @@
+package com.dan.esr.core.storage;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Path;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("danfood.storage")
+public class StorageProperties {
+
+    private Local local = new Local();
+    private S3 s3 = new S3();
+
+    public Path diretorioLocalFotos() {
+        return this.getLocal().getDiretorioFotos();
+    }
+
+    @Getter
+    @Setter
+    public static class Local {
+        private Path diretorioFotos;
+    }
+
+    @Getter
+    @Setter
+    public static class S3 {
+        private String idChaveAcesso;
+        private String chaveAcessoSecreta;
+        private String bucket;
+        private String regiao;
+        private String diretorioFotos;
+
+    }
+}
