cd63b539e80c560075b9855e3ef39cab58a844f9
Danilo Rocha <danilo.rochaa@gmail.com>
2024-06-25 10:02:08 -0300

    Configura o uso de Shallow Entity Tags

--- src/main/java/com/dan/esr/core/web/WebConfig.java
+++ src/main/java/com/dan/esr/core/web/WebConfig.java
@@ -1,6 +1,9 @@
 package com.dan.esr.core.web;
 
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +12,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*");
+        registry.addMapping("/**")// Permite qualquer request origen
+                .allowedMethods("*"); // Permite qualquer método http
+                //.allowedOrigins("*") // Permite qualquer request origen
+                //.maxAge(30); // Tempo máximo de cache em segundos
+
+    }
+
+    /*
+    * Registro do filtro para Entity Tag
+    * */
+    @Bean
+    public Filter shawllowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
