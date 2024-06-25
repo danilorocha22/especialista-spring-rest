c7b2d11cb4bb0538b74803e4afb597e0013f1e30
Danilo Rocha <danilo.rochaa@gmail.com>
2024-06-25 10:01:28 -0300

    Consulta a última data de atualização de uma Forma de Pagamento

--- src/main/java/com/dan/esr/domain/repositories/FormasPagamentoRepository.java
+++ src/main/java/com/dan/esr/domain/repositories/FormasPagamentoRepository.java
@@ -1,6 +1,13 @@
 package com.dan.esr.domain.repositories;
 
 import com.dan.esr.domain.entities.FormaPagamento;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
 
 public interface FormasPagamentoRepository extends CustomBaseJpaRepository<FormaPagamento, Long> {
+
+    @Query("select max(dataAtualizacao) from FormaPagamento")
+    Optional<OffsetDateTime> getDataAtualizacaoMaisRecente();
 }
