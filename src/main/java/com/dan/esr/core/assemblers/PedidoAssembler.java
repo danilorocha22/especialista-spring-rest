a0df4f1c0c87ace65f944a4ea9be128c0f9f43ee
Danilo Rocha <danilo.rochaa@gmail.com>
2024-07-09 21:04:36 -0300

    Adicionando links condicionalmente

--- src/main/java/com/dan/esr/core/assemblers/PedidoAssembler.java
+++ src/main/java/com/dan/esr/core/assemblers/PedidoAssembler.java
@@ -1,10 +1,6 @@
 package com.dan.esr.core.assemblers;
 
-import com.dan.esr.api.controllers.formapagamento.FormaPagamentoController;
 import com.dan.esr.api.controllers.pedido.PedidoPesquisaController;
-import com.dan.esr.api.controllers.restaurante.RestaurantePesquisaController;
-import com.dan.esr.api.controllers.usuario.UsuarioPesquisaController;
-import com.dan.esr.api.helper.links.Links;
 import com.dan.esr.api.models.input.pedido.PedidoInput;
 import com.dan.esr.api.models.output.pedido.PedidoOutput;
 import com.dan.esr.api.models.output.pedido.PedidoStatusOutput;
@@ -13,14 +9,10 @@ import lombok.NonNull;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
-import static com.dan.esr.api.helper.links.factory.TemplateVariablesFactory.*;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static com.dan.esr.api.helper.links.Links.*;
 
 
 @Component
@@ -37,25 +29,29 @@ public class PedidoAssembler extends RepresentationModelAssemblerSupport<Pedido,
     public PedidoOutput toModel(@NonNull Pedido pedido) {
         PedidoOutput pedidoOutput = createModelWithId(pedido.getCodigo(), pedido);
         this.mapper.map(pedido, pedidoOutput);
+        pedidoOutput
+                .add(linkToRestaurante(pedido.getRestaurante().getId()))
+                .add(linkToFormaPagamento(pedido.getFormaPagamento().getId()))
+                .add(linkToUsuario(pedido.getUsuario().getId()))
+                .addIf(pedido.podeSerConfirmado(), () -> linkToPedidoConfirmado(pedido.getCodigo()))
+                .addIf(pedido.podeSerEntregue(), () -> linkToPedidoEntregue(pedido.getCodigo()))
+                .addIf(pedido.podeSerCancelado(), () -> linkToPedidoCancelado(pedido.getCodigo()))
+                .getItensPedido().forEach(item ->
+                        item.add(linkToProduto(pedido.getRestaurante().getId(), item.getProdutoId())));
 
-        return pedidoOutput
-                .add(Links.linkToPedidos())
-                .add(linkTo(methodOn(RestaurantePesquisaController.class).restaurante(pedido.getId())).withSelfRel())
-                .add((linkTo(methodOn(FormaPagamentoController.class).formaPagamento(
-                        pedido.getFormaPagamento().getId(), null)).withSelfRel()))
-                .add(linkTo(methodOn(UsuarioPesquisaController.class).usuario(pedido.getUsuario().getId()))
-                        .withSelfRel());
+        return pedidoOutput;
     }
 
     public PedidoStatusOutput toModelStatus(Pedido pedido) {
-        return this.mapper.map(pedido, PedidoStatusOutput.class);
+        PedidoOutput pedidoOutput = toModel(pedido);
+        PedidoStatusOutput pedidoStatusOutput = this.mapper.map(pedido, PedidoStatusOutput.class);
+        return pedidoStatusOutput.add(pedidoOutput.getLinks());
     }
 
     @NonNull
     @Override
     public CollectionModel<PedidoOutput> toCollectionModel(@NonNull Iterable<? extends Pedido> entities) {
-        return toCollectionModel(entities)
-                .add(linkTo(PedidoPesquisaController.class).withRel("pedidos"));
+        return super.toCollectionModel(entities).add(linkToPedidos());
     }
 
     public Pedido toDomain(PedidoInput pedidoInput) {
