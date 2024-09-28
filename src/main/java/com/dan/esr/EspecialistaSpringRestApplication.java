3255bab1ada038830566da17d1085462b6d2dcac
Danilo Rocha <danilo.rochaa@gmail.com>
2024-09-28 18:30:08 -0300

    Externalizando o KeyStore criando um ProtocolResolver para Base64

--- src/main/java/com/dan/esr/EspecialistaSpringRestApplication.java
+++ src/main/java/com/dan/esr/EspecialistaSpringRestApplication.java
@@ -1,10 +1,10 @@
 package com.dan.esr;
 
+import com.dan.esr.core.io.Base64ProtocolResolver;
 import com.dan.esr.infrastructure.repositories.impl.CustomBaseJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.TimeZone;
 
@@ -13,8 +13,12 @@ import java.util.TimeZone;
 @EnableJpaRepositories(repositoryBaseClass = CustomBaseJpaRepositoryImpl.class) // Habilita o repositório customizado
 public class EspecialistaSpringRestApplication {
 
-	public static void main(String[] args) {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));// Configura o horário default da aplicação para UTC
-		SpringApplication.run(EspecialistaSpringRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));// Configura o horário default da aplicação para UTC
+        SpringApplication.run(EspecialistaSpringRestApplication.class, args);
+
+        /*var app = new SpringApplication(EspecialistaSpringRestApplication.class);
+        app.addListeners(new Base64ProtocolResolver());
+        app.run(args);*/
+    }
 }
